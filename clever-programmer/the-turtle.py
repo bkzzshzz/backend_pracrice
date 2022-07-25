b6941a5d4fa3195fe601a730d7718ad485a445cf
Bikesh Shrestha <bkzz_shzz@yahoo.com>
2022-07-25 09:56:33 +0545

    Introduction to turtle | Square

---
+++ clever-programmer/the-turtle.py
@@ -0,0 +1,12 @@
+import turtle
+
+my_turtle = turtle.Turtle()
+wn = turtle.Screen()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+wn.exitonclick()
